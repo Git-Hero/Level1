910653066d30ee61c9cec4137855e470b4933438
jeevansai <jeevansai502@gmail.com>
2016-04-03 21:11:27 +0530

    Update binary_search_rec.cpp

--- binary_search_rec.cpp
+++ binary_search_rec.cpp
@@ -1,15 +1,16 @@
 // !/usr/bin/g++
 // Searches for an element using binary search
 #include <iostream>
+using namespace std;
 
 int binsearch(int *arr, int l, int r, int val)
 {
-	int mid = (l + (r - l)) / 2
+	int mid = ( (r - l)) / 2;
 	if (arr[mid] == val)
 		return mid+1;
 	if (l > r)
 		return -1;
-	if (arr[mid] >= var)
+	if (arr[mid] >= val)
 		binsearch(arr, mid+1, r, val);
 	else
 		binsearch(arr, l, mid-1, val);
@@ -17,17 +18,17 @@ int binsearch(int *arr, int l, int r, int val)
 
 int main()
 {
-	int t, n, var i;
+	int t, n, val, i;
 	cin >> t;
 	while (t--) {
 		cin >> n;
-		int a[n];
-		for (i = 0; i <= n; i++);
+		int arr[n];
+		for (i = 0; i < n; i++)
 			cin >> arr[i];
 
 		cin >> val;
-		cout << binsearch(a, 0, n, val); << endl;
+		cout << binsearch(arr, 0, n, val) << endl;
 	}
-}
+
 	return 0;
 }
