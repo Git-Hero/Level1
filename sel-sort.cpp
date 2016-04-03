eb97607c380b58bb3a66d21a1187f8c3cdedf86f
ankushjangid7 <iit2014119@iiita.ac.in>
2016-04-03 21:56:09 +0530

    Update sel-sort.cpp

--- sel-sort.cpp
+++ sel-sort.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
-#include <ctime>
-#include <iomanip>
 using namespace std;
-
+ 
 //sorts using insertion sort
-
+ 
 void sort(int *arr, int n)
 {
-	int i, j, temp, min;
-	for (i=1; i<n-1; i++) {
-		temp=arr[j];
-		min = i;
-		for (j = i+1; j < n; j++) {
-			if (arr[min] > arr[j])
-				min = j;
-		}
-		arr[j] = arr[min];
-		arr[min] = tmp;
-	}
+	int i, j, temp;
+	for (i = 1 ; i <= n - 1; i++) {
+	    j = i;
+	    while ( j > 0 && arr[j] < arr[j-1]) {
+    		temp = arr[j];
+                arr[j] = arr[j-1];
+        	arr[j-1] = temp;
+		j--;
+    	    }
+    	}
 }
-
+ 
 void getdata(int *arr, int n)
 {
-	for (int i=0; i<n; i++){
+	for (int i=0; i<=n; i++){
 		cin>>arr[i];
 	}
 }
-
+ 
 void printdata(int *arr,int n) {
 	for (int i=0;i<n;i++) {
 		cout << arr[i] << " " ;
 	}
-	cout << endl
+	cout << endl;
 }
-
+ 
 int main()
 {
 	int t;
